cedfab3a484232d73f3396303297917221565aab
Ludhanna <ludhanna.veras@unesp.br>
2021-04-15 21:34:02 -0300

    Initial commit

---
+++ Instrucao.R
@@ -0,0 +1,7 @@
+#usethis::create_project("C:\\ProjetosR\\lab_ciencia_dados")
+
+# Criar  estrutura GIT para este projeto
+usethis::use_git()
+usethis::use_github()
+usethis::create_github_token()
+gitcreds::gitcreds_set()
