58a743caa92be38978c6da9bc64e204479ec94d5
Ludhanna <ludhanna.veras@unesp.br>
2021-04-15 21:50:58 -0300

    Commit1

--- Instrucao.R
+++ Instrucao.R
@@ -4,4 +4,5 @@
 usethis::use_git()
 usethis::use_github()
 usethis::create_github_token()
-gitcreds::gitcreds_set()
+gitcreds::gitcreds_set(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)
+gitcreds::gitcreds_set(url = "http://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")
